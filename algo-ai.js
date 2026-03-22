709839edaa4ccf2e9fd8924f95362bb39e617cdd
nakada toyohisa <nakada@nuis.ac.jp>
2026-03-22 16:29:39 +0900

    カードの数値で可能性があるものを列挙する。但し、相手がコールした数値については考慮していない。考慮するとすると、相手のコールが間違える（例えばカードを持っているのにその数値を言う）を考慮しなければいけないため）

--- algo-ai.js
+++ algo-ai.js
@@ -6,6 +6,46 @@ const PLAYER_IDS = [-1, 1];
 const CARD_COLORS = ["black", "white"];
 const CARD_NUMBERS = [...Array(12).keys()];
 const ACTION_TYPES = ["attack", "stay"];
+function getActions(state) {
+    const toSerial = (value, color) => value * 2 + (color === "black" ? 0 : 1);
+    // カードの数値は、黒は*2、白は*2+1として扱う
+    const remainedCardNumbers = [
+        ...state.playerHands[state.currentPlayerId],
+        ...state.playerHands[state.currentEnemyId].filter(card => card.isFaceUp)
+    ].map((card) => toSerial(card.number, card.color));
+    // console.log(remainedCardNumbers);
+    const targetHand = state.playerHands[state.currentEnemyId];
+    const possibleNumbers = [...Array(targetHand.length)];
+    let value = 0;
+    for (let card_i = 0; card_i < targetHand.length; card_i += 1) {
+        const card = targetHand[card_i];
+        const called = state.called[state.currentPlayerId].filter(action => action.targetCard?.id === card.id).
+            map(action => toSerial(action.targetNumber, action.targetCard.color));
+        if (card.isFaceUp) {
+            possibleNumbers[card_i] = [toSerial(card.number, card.color)];
+        }
+        else {
+            possibleNumbers[card_i] = CARD_NUMBERS.flatMap(v => [v * 2, v * 2 + 1]).filter(v => v >= value && v % 2 === (card.color === "black" ? 0 : 1) &&
+                !remainedCardNumbers.includes(v) && !called.includes(v));
+        }
+        value = possibleNumbers[card_i][0] + 1;
+    }
+    value = CARD_NUMBERS.at(-1) * 2 + 1;
+    for (let card_i = targetHand.length - 1; card_i >= 0; card_i -= 1) {
+        const card = targetHand[card_i];
+        if (!card.isFaceUp) {
+            possibleNumbers[card_i] = possibleNumbers[card_i].filter(v => v <= value);
+        }
+        value = possibleNumbers[card_i].at(-1) - 1;
+    }
+    for (let card_i = 0; card_i < targetHand.length; card_i += 1) {
+        const card = targetHand[card_i];
+        possibleNumbers[card_i] = possibleNumbers[card_i].map(v => (v - (card.color === "black" ? 0 : 1)) / 2);
+    }
+    // console.log(possibleNumbers)
+    return targetHand.map((card, card_i) => [card, possibleNumbers[card_i]]).filter(e => !e[0].isFaceUp).
+        flatMap(([card, p_numbers]) => p_numbers.map(v => ({ type: "attack", targetCard: card, targetNumber: v })));
+}
 class Game {
     judge(state) {
         return PLAYER_IDS.reduce((judge, id) => state.playerHands[id].every(card => card.isFaceUp) ? id * -1 : judge, 0);
@@ -19,20 +59,21 @@ class Game {
         return JSON.stringify(state0) === JSON.stringify(state1);
     }
     actions(state) {
-        const targetCards = state.playerHands[state.currentEnemyId].filter((e) => !e.isFaceUp);
-        const remainedCardNumbers = CARD_COLORS.reduce((a, color) => {
-            const opened = Object.entries(state.playerHands).flatMap(([playerId, cards]) => {
-                const isMe = Number(playerId) === state.currentPlayerId;
-                return cards.filter(card => card.color === color && (isMe || card.isFaceUp)).map(card => card.number);
-            });
-            const remained = CARD_NUMBERS.filter(e => !opened.includes(e));
-            return { ...a, [color]: remained };
-        }, {});
-        // ToDo: cardの並び順でありえない数字、また、自分と相手がコールした数字をここでは考慮していない。
         // ToDo: 山札から引いたカード(drawnCard)をアタック失敗で相手にさらすリスクについても試算していない
+        const actions = getActions(state);
+        // console.log(actions)
+        // const targetCards = state.playerHands[state.currentEnemyId].filter((e: Card) => !e.isFaceUp);
+        // const remainedCardNumbers = CARD_COLORS.reduce((a, color) => {
+        //     const opened = Object.entries(state.playerHands).flatMap(([playerId, cards]) => {
+        //         const isMe = Number(playerId) === state.currentPlayerId;
+        //         return (cards as Card[]).filter(card => card.color === color && (isMe || card.isFaceUp)).map(card => card.number);
+        //     });
+        //     const remained = CARD_NUMBERS.filter(e => !opened.includes(e));
+        //     return { ...a, [color as CardColor]: remained }
+        // }, {} as Record<CardColor, number[]>);
         // console.log(state);
         // console.log(targetCards.flatMap(card => remainedCardNumbers[card.color].map(n => ({ targetCard: card, targetNumber: n }))))
-        const actions = targetCards.flatMap(card => remainedCardNumbers[card.color].map(n => ({ type: "attack", targetCard: card, targetNumber: n })));
+        // const actions: Action[] = targetCards.flatMap(card => remainedCardNumbers[card.color].map(n => ({ type: "attack", targetCard: card, targetNumber: n })));
         if (state.firstAttack === false) {
             actions.push({ type: "stay" });
         }
@@ -47,8 +88,6 @@ class Game {
                 state2.firstAttack = false;
             }
             else {
-                // console.log("++ state",state2)
-                // console.log("++ action",action)
                 state2.playerHands[state2.currentPlayerId].filter(card => card.id === state2.drawnCard.id)[0].isFaceUp = true;
                 state2.currentPlayerId = state.currentEnemyId;
                 state2.currentEnemyId = state.currentPlayerId;
@@ -92,18 +131,25 @@ class Game {
     }
 }
 function next_ai_random(state) {
-    // const targets = state.playerHands[state.currentEnemyId].filter(c => !c.isFaceUp);
-    // return { type: "attack", targetCard: targets[Math.floor(Math.random() * targets.length)]!, targetNumber: Math.floor(Math.random() * 12) };
     const actions = mtcs.game.actions(state);
     return actions[Math.floor(actions.length * Math.random())];
 }
 import { mtcs } from "./mtcs.js";
 mtcs.game = new Game();
+let global_called = null;
+export function init_game() {
+    global_called = PLAYER_IDS.reduce((a, e) => ({ ...a, [e]: [] }), {});
+}
 export function next_ai(state, drawnCard) {
-    state.called = PLAYER_IDS.reduce((a, e) => ({ ...a, [e]: [] }), {});
+    if (global_called === null) {
+        throw new Error("please call init_game function before calling next_ai");
+    }
+    state.called = global_called;
     // return next_ai_random(state);
-    const b = next_ai_random(Game.addCardToHand(state, drawnCard));
+    // const b = next_ai_random(Game.addCardToHand(state, drawnCard));
     const a = mtcs.next_ai(Game.addCardToHand(state, drawnCard), state.currentPlayerId, 500);
-    console.log(a, b);
+    // console.log(a, b);
+    global_called[state.currentPlayerId].push(a);
+    // console.log(b);
     return a;
 }
--- algo-ai.ts
+++ algo-ai.ts
@@ -35,6 +35,49 @@ type State = {
     called: Record<PlayerId, Action[]>;
 }
 
+function getActions(state: State): Action[] {
+    const toSerial = (value: number, color: "black" | "white") => value * 2 + (color === "black" ? 0 : 1);
+    // カードの数値は、黒は*2、白は*2+1として扱う
+    const remainedCardNumbers = [
+        ...state.playerHands[state.currentPlayerId],
+        ...state.playerHands[state.currentEnemyId].filter(card => card.isFaceUp)
+    ].map((card) => toSerial(card.number, card.color));
+    // console.log(remainedCardNumbers);
+
+    const targetHand = state.playerHands[state.currentEnemyId];
+    const possibleNumbers: number[][] = [...Array(targetHand.length)];
+    let value = 0;
+    for (let card_i = 0; card_i < targetHand.length; card_i += 1) {
+        const card = targetHand[card_i]!;
+        const called = state.called[state.currentPlayerId].filter(action => action.targetCard?.id === card.id).
+            map(action => toSerial(action.targetNumber!, action.targetCard!.color));
+        if (card.isFaceUp) {
+            possibleNumbers[card_i]! = [toSerial(card.number, card.color)];
+        } else {
+            possibleNumbers[card_i]! = CARD_NUMBERS.flatMap(v => [v * 2, v * 2 + 1]).filter(v =>
+                v >= value && v % 2 === (card.color === "black" ? 0 : 1) &&
+                !remainedCardNumbers.includes(v) && !called.includes(v)
+            );
+        }
+        value = possibleNumbers[card_i]![0]! + 1;
+    }
+    value = CARD_NUMBERS.at(-1)! * 2 + 1;
+    for (let card_i = targetHand.length - 1; card_i >= 0; card_i -= 1) {
+        const card = targetHand[card_i]!;
+        if (!card.isFaceUp) {
+            possibleNumbers[card_i]! = possibleNumbers[card_i]!.filter(v => v <= value);
+        }
+        value = possibleNumbers[card_i]!.at(-1)! - 1;
+    }
+    for (let card_i = 0; card_i < targetHand.length; card_i += 1) {
+        const card = targetHand[card_i]!;
+        possibleNumbers[card_i] = possibleNumbers[card_i]!.map(v => (v - (card.color === "black" ? 0 : 1)) / 2);
+    }
+    // console.log(possibleNumbers)
+    return targetHand.map((card, card_i) => [card, possibleNumbers[card_i]]).filter(e => !(e[0]! as Card).isFaceUp).
+        flatMap(([card, p_numbers]) => (p_numbers! as number[]).map(v => ({ type: "attack", targetCard: card, targetNumber: v } as Action)));
+}
+
 class Game {
     judge(state: State) {
         return PLAYER_IDS.reduce((judge, id) => state.playerHands[id].every(card => card.isFaceUp) ? id * -1 : judge, 0);
@@ -48,20 +91,22 @@ class Game {
         return JSON.stringify(state0) === JSON.stringify(state1);
     }
     actions(state: State): Action[] {
-        const targetCards = state.playerHands[state.currentEnemyId].filter((e: Card) => !e.isFaceUp);
-        const remainedCardNumbers = CARD_COLORS.reduce((a, color) => {
-            const opened = Object.entries(state.playerHands).flatMap(([playerId, cards]) => {
-                const isMe = Number(playerId) === state.currentPlayerId;
-                return (cards as Card[]).filter(card => card.color === color && (isMe || card.isFaceUp)).map(card => card.number);
-            });
-            const remained = CARD_NUMBERS.filter(e => !opened.includes(e));
-            return { ...a, [color as CardColor]: remained }
-        }, {} as Record<CardColor, number[]>);
-        // ToDo: cardの並び順でありえない数字、また、自分と相手がコールした数字をここでは考慮していない。
         // ToDo: 山札から引いたカード(drawnCard)をアタック失敗で相手にさらすリスクについても試算していない
+        const actions: Action[] = getActions(state);
+        // console.log(actions)
+
+        // const targetCards = state.playerHands[state.currentEnemyId].filter((e: Card) => !e.isFaceUp);
+        // const remainedCardNumbers = CARD_COLORS.reduce((a, color) => {
+        //     const opened = Object.entries(state.playerHands).flatMap(([playerId, cards]) => {
+        //         const isMe = Number(playerId) === state.currentPlayerId;
+        //         return (cards as Card[]).filter(card => card.color === color && (isMe || card.isFaceUp)).map(card => card.number);
+        //     });
+        //     const remained = CARD_NUMBERS.filter(e => !opened.includes(e));
+        //     return { ...a, [color as CardColor]: remained }
+        // }, {} as Record<CardColor, number[]>);
         // console.log(state);
         // console.log(targetCards.flatMap(card => remainedCardNumbers[card.color].map(n => ({ targetCard: card, targetNumber: n }))))
-        const actions: Action[] = targetCards.flatMap(card => remainedCardNumbers[card.color].map(n => ({ type: "attack", targetCard: card, targetNumber: n })));
+        // const actions: Action[] = targetCards.flatMap(card => remainedCardNumbers[card.color].map(n => ({ type: "attack", targetCard: card, targetNumber: n })));
         if (state.firstAttack === false) {
             actions.push({ type: "stay" });
         }
@@ -75,8 +120,6 @@ class Game {
                 state2.playerHands[state2.currentEnemyId].filter(card => card.id === action.targetCard!.id)[0]!.isFaceUp = true;
                 state2.firstAttack = false;
             } else {
-                // console.log("++ state",state2)
-                // console.log("++ action",action)
                 state2.playerHands[state2.currentPlayerId].filter(card => card.id === state2.drawnCard!.id)[0]!.isFaceUp = true;
                 state2.currentPlayerId = state.currentEnemyId;
                 state2.currentEnemyId = state.currentPlayerId;
@@ -122,20 +165,28 @@ class Game {
 }
 
 function next_ai_random(state: State): Action {
-    // const targets = state.playerHands[state.currentEnemyId].filter(c => !c.isFaceUp);
-    // return { type: "attack", targetCard: targets[Math.floor(Math.random() * targets.length)]!, targetNumber: Math.floor(Math.random() * 12) };
     const actions = mtcs.game.actions(state);
     return actions[Math.floor(actions.length * Math.random())];
 }
 
 import { mtcs } from "./mtcs.js";
 mtcs.game = new Game();
+let global_called: Record<PlayerId, Action[]> | null = null;
+
+export function init_game() {
+    global_called = PLAYER_IDS.reduce((a, e) => ({ ...a, [e]: [] }), {} as Record<PlayerId, Action[]>);
+}
 
 export function next_ai(state: State, drawnCard: Card): Action {
-    state.called = PLAYER_IDS.reduce((a, e) => ({ ...a, [e]: [] }), {} as Record<PlayerId, Action[]>)
+    if (global_called === null) {
+        throw new Error("please call init_game function before calling next_ai");
+    }
+    state.called = global_called;
     // return next_ai_random(state);
-    const b = next_ai_random(Game.addCardToHand(state, drawnCard));
+    // const b = next_ai_random(Game.addCardToHand(state, drawnCard));
     const a = mtcs.next_ai(Game.addCardToHand(state, drawnCard), state.currentPlayerId, 500);
-    console.log(a, b);
+    // console.log(a, b);
+    global_called[state.currentPlayerId].push(a);
+    // console.log(b);
     return a;
 }
